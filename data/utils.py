8e01626d531d565087bde3c2b1534454224929cf
Florian Albrecht <florian.albrecht.public@outlook.de>
2022-12-11 23:51:29 +0100

    Updates to the Decision tree and random forrest hyper parameters to always ensure balanced training

--- data/utils.py
+++ data/utils.py
@@ -1,4 +1,6 @@
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import RocCurveDisplay
+import matplotlib.pyplot as plt
 
 def split_train_val_test(data_set, validation_size = 0.10, test_size = 0.20, random_state = 42):
     assert len(data_set) != 0
@@ -8,3 +10,18 @@ def split_train_val_test(data_set, validation_size = 0.10, test_size = 0.20, ran
         return temp_set, [], test_set
     train_set, validation_set = train_test_split(temp_set, test_size=validation_size/(1.0-test_size), random_state=random_state)
     return train_set, validation_set, test_set
+
+def plot_roc(ground_truth_labels, prediction_labels, title: str):
+    RocCurveDisplay.from_predictions(
+        ground_truth_labels,
+        prediction_labels,
+        name=f"class_of_interest vs the rest",
+        color="darkorange",
+    )
+    plt.plot([0, 1], [0, 1], "k--", label="chance level (AUC = 0.5)")
+    # plt.axis("square")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title(title)
+    plt.legend()
+    plt.show()
